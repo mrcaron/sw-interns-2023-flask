04d839169c959fd506e1d5fc861b3ed892894af1
Michael Caron <mrcaron@users.noreply.github.com>
2022-12-16 17:04:55 +0000

    Adds some todos and a books db

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+# TODO Add two numbers, 'first' and 'second' as query parameters
+# Ex: /add?first=4&second=8 => Returns "12"
+
+# TODO SOLIDWORKS-, 'x' and 'y' as query parameters, repeat "SOLID" x times and "WORKS" y times
+# Ex: /repeat?x=1&y=1 => Returns "SOLIDWORKS"
+
+# TODO Look up a book by ID
+# Ex: /book/2 => Returns full json object for "100 Years..."
+
+# TODO Return a summary of book stats
+# (total books, oldest release year, how many checked out?)
